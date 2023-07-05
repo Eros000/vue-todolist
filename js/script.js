6cfacc846621a2d5476f92685b31039d8e828e94
Eros000 <e.ghilardi@hotmail.it>
2023-07-05 08:44:15 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -3,10 +3,24 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-    
+        todos: [
+
+            {
+                newTodo: '',
+                done: false
+            },
+
+            
+
+        ]
       }
     },
     method:{
-
+        addTodo() {
+            if (this.newTodo.trim() != '') {
+                this.todos.push(this.newTodo);
+                this.newTodo = '' ;
+            }
+        }
     }
   }).mount('#app')
